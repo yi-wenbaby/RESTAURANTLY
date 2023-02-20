962f5c13d50526291f68afd3556bb7f8855834c7
yi-wenbaby <wen91502@yahoo.com.tw>
2023-02-20 14:26:34 +0800

    0220

---
+++ blog/connect .php
@@ -0,0 +1,38 @@
+<?php
+      $data1=$_POST['name'];
+      $data2=$_POST['email'];
+      $data3=$_POST['subject'];
+      $data4=$_POST['message'];
+
+//資料庫登錄權限驗證
+
+        $host = 'sql109.clouds.tw';
+        $db_name = 'clds_33581291_msg';
+        $db_user = 'clds_33581291'; 
+        $db_password = 's8908004491';
+
+        $con = mysqli_connect($host, $db_user, $db_password, $db_name);
+            if (!$con) {
+    die("權限連線錯誤！" . mysqli_connect_error());
+
+}
+
+//資料庫是否正常使用
+
+if(!mysqli_select_db($con, $db_name)) {
+    die ("資料庫連線異常！");
+  }
+  
+  mysqli_query( $con, "set names 'utf8'");
+
+//執行SQL語法
+
+//執行SQL語法
+
+$sql ="INSERT INTO `msg` (`id`, `name`, `email`,`subject`, `message`) VALUES (NULL, '$data1', '$data2', '$data3','$data4')";
+$result = mysqli_query($con, $sql);
+
+echo "感謝你的回覆!";
+header('Refresh:5; url=blog_index.html');
+
+?>
